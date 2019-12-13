591a51c98e7b8f2b60422a252b5082b739259a79
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-13 15:45:33 +0800

    Create userDrawer.jsx

---
+++ components/userDrawer/userDrawer.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './userDrawer.scss';
+import { Button, Drawer, Menu, Icon,Divider } from 'antd';
+import UserPopover from '../userPopover/userPopover';
+
+class userDrawer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible: false,
+            userMenu: [
+                { mid: '0', name: '收藏列表', icon: 'heart', twoToneColor: '#eb2f96' },
+                { mid: '1', name: '笔记本', icon: 'book', twoToneColor: '#9E7D52' },
+                { mid: '2', name: '提交记录', icon: 'clock-circle', twoToneColor: '#90D2FB' },
+                { mid: '3', name: '退出', icon: 'logout', twoToneColor: null }
+            ],
+            maniMenu:[
+                { mid: '0', name: '写文章', icon: 'edit', twoToneColor: null },
+                { mid: '1', name: '消息', icon: 'bell', twoToneColor: null },
+                { mid: '2', name: '切换为英文版', icon: 'swap', twoToneColor: null },
+            ]
+        };
+        this.showDrawer = this.showDrawer.bind(this);
+        this.onClose = this.onClose.bind(this);
+    }
+
+    showDrawer() {
+        this.setState({
+            visible: true
+        });
+    }
+
+    onClose() {
+        this.setState({
+            visible: false
+        });
+    }
