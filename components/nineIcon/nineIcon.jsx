abe669192b522e8f0e815ef32d56a411a63affcd
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-13 15:41:12 +0800

    Create nineIcon.jsx

---
+++ components/nineIcon/nineIcon.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './nineIcon.scss';
+
+import { Avatar } from 'antd';
+
+class nineIcon extends React.Component{
+    render(){
+        return (
+            <div style={this.props.style}>
+                <Avatar
+                    size="small"
+                    shape="square"
+                    src="./user/xiao-li-qwd/avatar/2019-12-06_152724.png"
+                    icon="user"
+                    style={this.props.avaStyle}
+                />
+                <span
+                    style={{display:'inline-block', marginLeft: '6px' }}>
+                    小九
+                </span>
+            </div>
+        )
+    }
+}
+
+export default nineIcon;
