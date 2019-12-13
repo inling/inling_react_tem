4c6f42cf8b7cb314434b3be54783d28cffc05d57
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-13 15:29:43 +0800

    Create iSider.jsx

---
+++ parts/iSider/iSider.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './iSider.scss';
+import {Layout} from 'antd';
+
+const {Sider} = Layout;
+class iSider extends React.Component{
+    render(){
+        return (
+            <Sider></Sider>
+        )
+    }
+}
+export default iSider;
