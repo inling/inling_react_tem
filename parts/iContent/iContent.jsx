b9a02b8ed29c04f694b54fe30da708faf9bb0a9e
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-13 15:27:17 +0800

    Create iContent.jsx

---
+++ parts/iContent/iContent.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './iContent.scss';
+import { List, Avatar, Layout } from 'antd';
+
+const {Content} = Layout
+class iContent extends React.Component {
+    render() {
+        const data = [
+            {
+                title: 'Ant Design Title 1',
+            },
+            {
+                title: 'Ant Design Title 2',
+            },
+            {
+                title: 'Ant Design Title 3',
+            },
+            {
+                title: 'Ant Design Title 4',
+            },
+        ];
+
+        return (
+            <Content style={{padding: '0 24px', minHeight:'280px'}}>
+                <List
+                    itemLayout="horizontal"
+                    dataSource={data}
+                    renderItem={item => (
+                        <List.Item>
+                            <List.Item.Meta
+                                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                title={<a href="https://ant.design">{item.title}</a>}
+                                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                            />
+                        </List.Item>
+                    )}
+                />
+            </Content>
+        )
+    }
+}
+export default iContent;
