4eabb85c0ebacb4dba3ac5833174c85baac640e4
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-20 16:46:52 +0800

    Update iSider.jsx

--- parts/iSider/iSider.jsx
+++ parts/iSider/iSider.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import './iSider.scss';
-import {Layout,Divider} from 'antd';
-
+import {Layout,Card, Icon, Avatar} from 'antd';
+const { Meta } = Card;
 const {Sider} = Layout;
 class iSider extends React.Component{
     render(){
         return (
-            <Sider className="slider" style={{background:'#fff',border:'1px solid red'}}>
-                <Divider type="vertical"/>
+            <Sider className="slider" 
+                style={{
+                        background:'#fff',
+                        border:'1px solid red'
+            }}>     
+
+                <Card
+                    style={{ width: 300 }}
+                    cover={
+                    <img
+                        alt="example"
+                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                    />
+                    }
+                    actions={[
+                    <Icon type="setting" key="setting" />,
+                    <Icon type="edit" key="edit" />,
+                    <Icon type="ellipsis" key="ellipsis" />,
+                    ]}
+                >
+                    <Meta
+                    avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                    title="Card title"
+                    description="This is the description"
+                    />
+                </Card>
             </Sider>
         )
     }
