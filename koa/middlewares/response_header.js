9b936f8aa4aa2248d45c18d6b555cc5940f0ee97
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-20 16:51:56 +0800

    Create response_header.js

---
+++ koa/middlewares/response_header.js
@@ -0,0 +1,11 @@
+//设置响应头 Response Header -跨域
+const response_header = async (ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    if (ctx.method == 'OPTIONS') {
+        ctx.body = 200;
+    } else {
+        await next();
+    }
+}
