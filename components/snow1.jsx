f0fab021fd9221f038983a4a8ad0aec588545722
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-20 16:31:32 +0800

    Create snow1.jsx

---
+++ components/snow1.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './snow.scss'
+class snow extends React.Component{
+    render(){
+        return (
+            <div className="snowBox">
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
+                <div className="snow"></div>
