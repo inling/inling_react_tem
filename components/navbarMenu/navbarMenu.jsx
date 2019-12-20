c5b2eb8053e8a7b62e073bae03ffd80c6d95481f
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-20 16:24:43 +0800

    1
    2

--- components/navbarMenu/navbarMenu.jsx
+++ components/navbarMenu/navbarMenu.jsx
@@ -3,32 +3,23 @@ import './navbarMenu.scss';
 import UserPopover from '../userPopover/userPopover';
 import Language from '../language/language';
 import Message from '../message/message';
-
-import { Button } from 'antd';
-
-function WriteBtn(props) {
-    return (
-        <span style={props.style}>
-            <Button icon="book">
-                写文章
-            </Button>
-        </span>
-    )
-}
+import PhoneApp from '../phoneApp/phoneApp';
 class navbarMenu extends React.Component {
     render() {
         return (
             <div className="navbarMenu" style={this.props.style}>
-                <WriteBtn style={{ marginRight: '48px' }} />
+                <PhoneApp 
+                    pAppPlacement="bottomRight"
+                    pAppTrigger="click"
+                    pAppStyle={{ marginRight: '16px', cursor: 'pointer',color:'white' }} />
                 <Message
                     mesPlacement="bottomRight"
                     mesTrigger="click"
-                    mesStyle={{ marginRight: '24px', cursor: 'pointer' }} />
+                    mesStyle={{ marginRight: '16px', cursor: 'pointer',color:'white' }} />
                 <Language style={{ marginRight: '24px', cursor: 'pointer' }} />
                 <UserPopover
                     popPlacement="bottomRight"
                     popTrigger="click"
-                    avaSize="small"
                     avaSrc="./user/xiao-li-qwd/avatar/20180424150900_3Erwd.jpeg"
                     avaStyle={{marginTop:'-2px'}}
                 />
