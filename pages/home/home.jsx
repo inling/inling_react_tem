19d2de48a00ed002b7857af524cde078d32d0b67
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-20 16:39:33 +0800

    Update home.jsx

--- pages/home/home.jsx
+++ pages/home/home.jsx
@@ -4,15 +4,21 @@ import { Layout } from 'antd';
 import IHeader from '../../parts/iHeader/iHeader';
 import IContent from '../../parts/iContent/iContent';
 import ISider from '../../parts/iSider/iSider';
+import IFooter from '../../parts/iFooter/iFooter';
+
+
+import Snow from '../../components/snow/snow';
 class home extends React.Component {
     render() {
         return (
-            <Layout style = {{ background: '#fff' }}>
+            <Layout style = {{ background: '#fff',minHeight:'100vh' }}>
                 <IHeader theme="light" />
                 <Layout className="contentAndSiderContainer">
                     <IContent />
                     <ISider />
                 </Layout>
+                <IFooter/>
+                <Snow />
             </Layout>
         )
     }
