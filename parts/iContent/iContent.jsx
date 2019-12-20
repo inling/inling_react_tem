d301369bd96e443e6d4192554c7158925caed66f
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-20 16:43:27 +0800

    Update iContent.jsx

--- parts/iContent/iContent.jsx
+++ parts/iContent/iContent.jsx
@@ -1,40 +1,28 @@
 import React from 'react';
 import './iContent.scss';
-import { List, Avatar, Layout } from 'antd';
+import { Layout,Carousel } from 'antd';
 
 const {Content} = Layout
 class iContent extends React.Component {
     render() {
-        const data = [
-            {
-                title: 'Ant Design Title 1',
-            },
-            {
-                title: 'Ant Design Title 2',
-            },
-            {
-                title: 'Ant Design Title 3',
-            },
-            {
-                title: 'Ant Design Title 4',
-            },
-        ];
-
         return (
-            <Content style={{padding: '0 24px', minHeight:'280px'}}>
-                <List
-                    itemLayout="horizontal"
-                    dataSource={data}
-                    renderItem={item => (
-                        <List.Item>
-                            <List.Item.Meta
-                                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                                title={<a href="https://ant.design">{item.title}</a>}
-                                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-                            />
-                        </List.Item>
-                    )}
-                />
+            <Content style={{padding: '0 24px', minHeight:'280px',background:'white'}}>
+                  <Carousel autoplay>
+                    <div>
+                    <h3>
+                        <img src="./image/690x388.jpg" alt="" style={{width:'100%',height:'280px'}}/>
+                    </h3>
+                    </div>
+                    <div>
+                    <h3>2</h3>
+                    </div>
+                    <div>
+                    <h3>3</h3>
+                    </div>
+                    <div>
+                    <h3>4</h3>
+                    </div>
+                </Carousel>
             </Content>
         )
     }
