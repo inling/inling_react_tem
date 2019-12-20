7cfd3ff07a1c0ee5a20c5322e1937bd3ab6af9ea
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-20 16:55:33 +0800

    Create token_errors.js

---
+++ koa/middlewares/token_errors.js
@@ -0,0 +1,14 @@
+//异常处理 Token-401
+const token_errors = async (ctx, next) => {
+    return next().catch((err) => {
+        if (err.status === 401) {
+            ctx.status = 401;
+            ctx.body = {
+                code: 401,
+                msg: err.message
+            }
+        } else {
+            throw err;
+        }
+    })
+}
