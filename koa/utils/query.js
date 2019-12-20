04b3f2a1ceb1348e83c67c44bc5495afe01626d4
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-20 16:57:10 +0800

    Create query.js

---
+++ koa/utils/query.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql');
+const MYSQL_CONFIG = require('../config/mysql_config');
+
+//创建连接池
+const pool = mysql.createPool(MYSQL_CONFIG);
+
+//sql查询
+const query = (sql, val, callback) => {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if (err) {
+                reject(err);
+            } else {
+                connection.query(sql, val, (err, res) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(callback(res));
+                    }
+                    connection.release();
+                })
+            }
+        })
+    })
+}
+
+module.exports = query;
