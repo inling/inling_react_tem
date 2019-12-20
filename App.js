7525b0c5fda6d13f1dd0914095b4ab0c1ba79d82
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-20 16:48:16 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.scss';
 import Home from './pages/home/home'
-
+import Login from './pages/login/login'
+import Register from './pages/register/register'
+import {BrowserRouter , Route, Switch} from 'react-router-dom';
 function App() {
   return (
     <div>
-      <Home />
+      <BrowserRouter >
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/login" component={Login}/>
+            <Register exact path="/register" component={Register}/>
+        </Switch>
+      </BrowserRouter >
     </div>
   );
 }
