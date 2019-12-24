2b2528e4ceaac09debc9b70408ec48fd55275630
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-24 16:59:05 +0800

    Create code_config.js

---
+++ koa/config/code_config.js
@@ -0,0 +1,10 @@
+const CODE_SUCCESS = {
+    login_success: { code: 0, message: '登陆成功' }
+}
+
+const CODE_FAILED = {
+    login_failed: { code: 100,message: '用户名或密码错误'},
+    nickname_occupied: { code: 101,message: '昵称已存在' }
+}
+
+/**v1.0.0 */
