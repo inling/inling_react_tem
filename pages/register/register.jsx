27f929f00a71d590d85a1f2df0675c86af7c0272
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-24 16:56:26 +0800

    Update register.jsx

--- pages/register/register.jsx
+++ pages/register/register.jsx
@@ -1,50 +1,37 @@
-import React from 'react';
-import { Input , Button} from 'antd'
-class register extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            uname:'',
-            upwd:''
-        }
-    }
-    handleClick=()=>{
-        let userInfo = {
-            uname:this.state.uname,
-            upwd:this.state.upwd
-        }
-        var token=localStorage.getItem('token')
-        fetch('http://127.0.0.1:3001/register',{
-            method:'POST',
-            headers:{
-                'Content-Type': 'application/json;charset=UTF-8',
-                'authorization':"Bearer "+token
-            },
-            mode:'cors',
-            body:JSON.stringify(userInfo)        
-        }).then(response=>{
-           
-        })
-    }
-    handleUnameChange=(e)=>{
-        this.setState({
-            uname:e.target.value
-        })
-    }
-    handleUpwdChange=(e)=>{
-        this.setState({
-            upwd:e.target.value
-        })
-    }
-    render(){
-        return(
-            <div style={{width:'200px'}}>
-                <Input value={this.state.uname} onChange={this.handleUnameChange}/>
-                <Input value={this.state.upwd} onChange={this.handleUpwdChange}/>
-                <Button onClick={this.handleClick}>注册</Button>
-            </div>
+<Content className="i-content register-content">
+                    <div>
+                        <div className="title-line">
+                            <span className="tit" style={{ fontSize: '38px' }}>注册</span>
+                        </div>
+                    </div>
+                    <ul className="login-info" style={{ margin: '60px auto 0', textAlign: 'center', listStyle: 'none', padding: 0 }}>
+                        <li>
+                            <Input  size="large" placeholder="昵称" value={this.state.nickname} onChange={this.handleNicknameChange} />
+                            <div style={{ height: '20px', margin: '6px 0' }}></div>
+                        </li>
+                        <li>
+                            <Input.Password size="large" placeholder="密码" visibilityToggle={false} value={this.state.upwd} onChange={this.handleUpwdChange} />
+                            <div style={{ height: '20px', margin: '6px 0' }}></div>
+                        </li>
+                        <li className="remember">
+                            <label>
+                                <input type="checkbox" />记住我
+						        <span>不是自己的电脑上不要勾选此项</span> 
+                                <a target="_blank" href="/" className="forget-password">忘记密码?</a> 
+                                <a target="_blank" href="/" className="not-checkout" style={{marginRight:'10px'}}>无法验证?</a>
+                            </label>
+                        </li>
+                        <li style={{display:'flex',justifyContent:'space-between'}}>
+                            <Button block type="primary" onClick={this.handleClick} size="large">注册</Button>
+                        </li>
+                    </ul>
+                </Content>
+                <IFooter />
+            </Layout>
         )
     }
 }
 
 export default register;
+
+/**v1.0.0 */
