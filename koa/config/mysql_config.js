429462e55cdd766d1a2556603b4328fe5f06d218
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-24 16:58:19 +0800

    Update mysql_config.js

--- koa/config/mysql_config.js
+++ koa/config/mysql_config.js
@@ -1,8 +1,9 @@
-const mysqlConfig = {
+exports.MYSQL_CONFIG = {
     host: '127.0.0.1',
     user: 'root',
     password: '',
     database: 'nine',
     connectionLimit: 10
 }
-module.exports = mysqlConfig;
+
+/**v1.0.0 */
