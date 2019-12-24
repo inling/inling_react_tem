eddd6ff017e4a130ace7560f0befcb117646b579
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-24 17:02:56 +0800

    Update user_router.js

--- koa/routes/user_router.js
+++ koa/routes/user_router.js
@@ -1,9 +1,11 @@
 const Router = require('koa-router');
 const user_controller = require('../controllers/user_controller');
-
 const router = new Router();
 
 router.post('/login', user_controller.login);
 router.post('/register', user_controller.register);
+router.get('/nickname',user_controller.nickname);
 
 module.exports = router;
+
+/**v1.0.0 */
