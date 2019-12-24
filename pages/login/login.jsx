ddf036b07e173c60ab8f25418e3cf23e26f48020
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-24 16:55:11 +0800

    Update login.jsx

--- pages/login/login.jsx
+++ pages/login/login.jsx
@@ -1,58 +1,46 @@
-import React from 'react';
-import { Input,Button,Layout } from 'antd';
-import IHeader from '../../parts/iHeader/iHeader';
-import IFooter from '../../parts/iFooter/iFooter';
-class login extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            uname:'',
-            upwd:''
-        }
-    }
-    handleClick=()=>{
-        let userInfo = {
-            uname:this.state.uname,
-            upwd:this.state.upwd
-        }
-        fetch('http://127.0.0.1:3001/login',{
-            method:'POST',
-            headers:{
-                'Content-Type': 'application/json;charset=UTF-8'
-            },
-            mode:'cors',
-            body:JSON.stringify(userInfo)        
-        }).then(response=>response.json()).then(res=>{
-            console.log(res);
-            localStorage.setItem('token',res.token)
-        })
-    }
-    handleUnameChange=(e)=>{
-        this.setState({
-            uname:e.target.value
-        })
-    }
-    handleUpwdChange=(e)=>{
-        this.setState({
-            upwd:e.target.value
-        })
-    }
-    render(){
-        return (
-            <Layout>
-                <IHeader />
-                <div style={{width:'200px'}}>
-                    <Input value={this.state.uname} onChange={this.handleUnameChange}/>
-                    <Input value={this.state.upwd} onChange={this.handleUpwdChange}/>
-                    <Button onClick={this.handleClick}>登陆</Button>
-                </div>
-                <IFooter/>
+<Content className="i-content login-content">
+                    <div>
+                        <div className="title-line">
+                            <span className="tit" style={{ fontSize: '38px' }}>登录</span>
+                        </div>
+                    </div>
+                    <ul className="login-info" style={{ margin: '60px auto 0', textAlign: 'center', listStyle: 'none', padding: 0 }}>
+                        <li>
+                            <Input className="error" size="large" placeholder="你的手机号/邮箱" value={this.state.nickname} onChange={this.handleNicknameChange} />
+                            <div style={{ height: '20px', margin: '6px 0' }}></div>
+                        </li>
+                        <li>
+                            <Input.Password size="large" placeholder="密码" value={this.state.upwd} onChange={this.handleUpwdChange} />
+                            <div style={{ height: '20px', margin: '6px 0' }}></div>
+                        </li>
+                        <li className="remember">
+                            <label>
+                                <input type="checkbox" />记住我
+						        <span>不是自己的电脑上不要勾选此项</span>
+                                <a target="_blank" href="/" className="forget-password">忘记密码?</a>
+                                <a target="_blank" href="/" className="not-checkout" style={{ marginRight: '10px' }}>无法验证?</a>
+                            </label>
+                        </li>
+                        <li style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <Button style={{ width: '45%' }} size="large" type="primary" onClick={this.handleClick}>登陆</Button>
+                            <Button style={{ width: '45%' }} size="large">注册</Button>
+                        </li>
+                        <li className="sns">
+                            <a href="/" style={{ marginRight: "20px", color: '#717171' }}>
+                                <Icon type="wechat" style={{ color: '#2EBB4F', fontSize: '18px', verticalAlign: 'middle', marginRight: '5px' }} />
+                                微信账号登录</a>
+                            <a href="/" style={{ color: '#717171' }}>
+                                <Icon type="qq" style={{ color: '#1890ff', fontSize: '18px', verticalAlign: 'middle', marginRight: '4px' }} />
+                                QQ账号登录</a>
+                        </li>
+                    </ul>
+                </Content>
+                <IFooter />
             </Layout>
-
-            
         )
-
     }
 }
 
 export default login;
+
+/**v1.0.0 */
