f5b80358d1a09c2e8c6c60b310289ab01ec7c6e5
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-27 16:53:22 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.scss';
-import Home from './pages/home/home'
-import Login from './pages/login/login'
-import Register from './pages/register/register'
+import 'antd/dist/antd.css';
+import '../assets/style/_color.scss';
+import '../assets/style/_common.scss';
+import '../assets/style/_reset.scss';
+import Home from './pages/home/home';
+import Login from './pages/login/login';
+import Register from './pages/register/register';
 import {BrowserRouter , Route, Switch} from 'react-router-dom';
 function App() {
   return (
-    <div>
+    <div style={{minWidth:'320px'}}>
       <BrowserRouter >
         <Switch>
             <Route exact path="/" component={Home}/>
             <Route exact path="/login" component={Login}/>
-            <Register exact path="/register" component={Register}/>
+            <Route exact path="/register" component={Register}/>
         </Switch>
       </BrowserRouter >
     </div>
@@ -19,3 +23,5 @@ function App() {
 }
 
 export default App;
+
+/**v1.0.0 */
