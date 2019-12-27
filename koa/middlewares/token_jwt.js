b4e274a7f0185c53ebed1ed27ab21dd118477c16
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-27 17:08:25 +0800

    Update token_jwt.js

--- koa/middlewares/token_jwt.js
+++ koa/middlewares/token_jwt.js
@@ -1,14 +1,14 @@
 const koajwt = require('koa-jwt');
-const { verToken } = require('../utils/token');
 const { TOKEN_SECRET } = require('../config/token_config');
+const { CODE_ARRAY } = require('../config/code_config');
 //Token 异常处理 401
 exports.token_errors = async (ctx, next) => {
     return next().catch((err) => {
         if (err.status === 401) {
             ctx.status = 401;
             ctx.body = {
-                code: 401,
-                msg: err.message
+                ...CODE_ARRAY.TOKEN_OVERTIME,
+                data: err.message      
             }
         } else {
             throw err;
@@ -16,23 +16,9 @@ exports.token_errors = async (ctx, next) => {
     })
 }
 
-//Token 验证
-exports.token_verify = async (ctx, next) => {
-    var token = ctx.headers.authorization;
-    if (token == undefined) {
-        await next();
-    } else {
-        var userInfo = verToken(token);
-        ctx.state = {
-            userInfo: userInfo
-        }
-        await next();
-    }
-}
-
 //Token 路由过滤
 exports.token_filter = koajwt({ secret: TOKEN_SECRET }).unless({
-    path: [/^\/login/,/^\/nickname/]
+    path: [/^\/login/,/^\/nickname/,/^\/register/,/^\/pk/]
 })
 
 /**v1.0.0 */
