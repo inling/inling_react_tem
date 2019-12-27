39a66425a622df4378da5aa34e34282f3cac8947
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-27 16:47:14 +0800

    Update login2.jsx

--- pages/login/login2.jsx
+++ pages/login/login2.jsx
@@ -1,55 +1,55 @@
-import React from 'react';
-import './login.scss';
-import { Input, Button, Layout, message, Icon } from 'antd';
-import IHeader from '../../parts/iHeader/iHeader';
-import IFooter from '../../parts/iFooter/iFooter';
+className={this.state.upwdInput ? null : 'error'}
+                                value={this.state.upwd} 
+                                onChange={this.handleUpwdChange} />
+                            <div className="err-message">
+                                {
+                                    !this.state.upwdInput && 
+                                        (this.state.loginCode === 0
+                                            ?<p className="tips">喵，你没输入密码么？</p>
+                                            :<p className="tips">账号或密码错误</p>)
+                                }
+                            </div>
+                        </li>
+                        <li className="remember">
+                            <label>
+                                <Checkbox className="me">记住我
+                                    <span className="bigscreen" style={{ color:' #bbb' ,marginLeft:'10px'}}>不是自己的电脑上不要勾选此项</span>
+                                </Checkbox>
+                                <Link target="_blank" to="/">忘记密码?</Link>
+                                <Link target="_blank" to="/" style={{ marginRight: '10px' }}>无法验证?</Link>
+                            </label>
+                        </li>
+                        <li className="btn-box">
+                            <Button
+                                size="large" 
+                                type="primary"
+                                style={{ width: '45%' }} 
+                                onClick={this.handleLoginClick}>
+                                登录
+                            </Button>
+                            <Button
+                                size="large"
+                                style={{ width: '45%' }}
+                                onClick={this.handleDirectClick}>
+                                注册
+                            </Button>
+                        </li>
+                        <li className="sns">
+                            <a href="/" className="wechat-href">
+                                <Icon type="wechat" />
+                                微信账号登录</a>
+                            <a href="/" className="qq-href">
+                                <Icon type="qq" />
+                                QQ账号登录</a>
+                        </li>
+                    </ul>
+                </Content>
+                <IFooter />
+            </Layout>
+        )
+    }
+}
 
-const { Content } = Layout;
-class login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            nickname: '',
-            upwd: ''
-        }
-    }
-    handleClick = () => {
-        let userInfo = {
-            nickname: this.state.nickname,
-            upwd: this.state.upwd
-        }
-        fetch('http://127.0.0.1:3001/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=UTF-8'
-            },
-            mode: 'cors',
-            body: JSON.stringify(userInfo)
-        }).then(response => response.json()).then(res => {
-            console.log(res);
-            if (res.code === 0) {
-                message.success('登陆成功');
-                localStorage.setItem('token', res.token)
-            } else {
-                message.error('登陆失败');
-            }
+export default login;
 
-        })
-    }
-    handleNicknameChange = e => {
-        this.setState({
-            nickname: e.target.value
-        })
-    }
-    handleUpwdChange = e => {
-        this.setState({
-            upwd: e.target.value
-        })
-    }
-    render() {
-        return (
-            <Layout style={{ background: 'white', minHeight: '100vh' }}>
-                <IHeader />
-                
-                
-    /**v1.0.0*/
+/**v1.0.0 */
