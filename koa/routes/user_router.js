ccabf54670f9cd52f6678d72dc3816deb63ea19b
inling.aup <43487924+inling@users.noreply.github.com>
2019-12-27 17:09:44 +0800

    Update user_router.js

--- koa/routes/user_router.js
+++ koa/routes/user_router.js
@@ -5,6 +5,8 @@ const router = new Router();
 router.post('/login', user_controller.login);
 router.post('/register', user_controller.register);
 router.get('/nickname',user_controller.nickname);
+router.get('/getUserInfo',user_controller.getUserInfo);
+router.post('/pk',user_controller.pk);
 
 module.exports = router;
 
